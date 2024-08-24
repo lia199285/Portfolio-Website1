c26205e7c80f05c846eb323a1346d4fb44dddefe
lia199285 <lia199285@gmail.com>
2024-08-24 19:24:32 -0400

    cambio
    cambios

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Nathalia's Portfolio")
+console.log ("Welcome to Nathalia's Portfolio.");
+function menuToggle(){
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle'){
+        x.className += ' responsive';
+    } else{
+        x.className = 'navtoggle';
+    }
+}
