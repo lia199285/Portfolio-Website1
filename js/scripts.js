fa5d15b00ac9127e59c3c86c54fd33dc1766118b
lia199285 <lia199285@gmail.com>
2024-08-29 20:05:25 -0400

    cambio
    cambio

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 console.log ("Welcome to Nathalia's Portfolio.");
-function menuToggle(){
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle'){
-        x.className += ' responsive';
-    } else{
-        x.className = 'navtoggle';
-    }
+
+function menuToggle(x) {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
+
+$('#click_advance').click(function() {
+    $('#display_advance').toggle('1000');
+    $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
